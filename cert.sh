8bfd47f972f8caa9f06b35f615f84928171606ab
Tory Kulick <30578137+tkulick@users.noreply.github.com>
2018-11-23 16:22:41 -0500

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -2,11 +2,8 @@
 # SSL Cert Generation Script with Let's Encrypt
 
 # Setup Process
-sudo apt-get -y update
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:certbot/certbot
-sudo apt-get update
-sudo apt-get install python-certbot-nginx 
+sudo apt-get -y update && sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:certbot/certbot && sudo apt-get -y update && sudo apt-get -y install python-certbot-nginx 
 
 # Cert Installation
 sudo certbot --nginx
